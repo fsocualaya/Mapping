8368834708dc184a44e8a9deae0175c15ce7e817
Nadnus <fernando.delosheros@utec.edu.pe>
2019-07-01 00:16:16 -0500

    Intersecciones con los bordes de voronoi

--- Qt2/graficar/test.cpp
+++ Qt2/graficar/test.cpp
@@ -2,6 +2,8 @@
 int main()
 {
     coordinate one(0, 0);
-    coordinate two(4, 4);
-    bisect(make_pair(one, two));
+    coordinate two(0, 4);
+    auto a = bisect(make_pair(one, two));
+    auto primero = a.first;
+    auto segundo = a.second;
 }
